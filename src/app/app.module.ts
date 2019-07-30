28bf16bbfd8da94b4c4dbf5cf78d2ed106b4f75e
Shomendra Pradhan <47145041+Shomendra@users.noreply.github.com>
2019-07-30 15:59:44 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from "./home/home.component";
-import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogViewComponent } from "./blog-view/blog-view.component";
 import { BlogCreateComponent } from './blog-create/blog-create.component';
 import { BlogEditComponent } from './blog-edit/blog-edit.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BlogService } from './blog.service';
+import { BlogHttpService } from './blog-http.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,7 @@ import { BlogService } from './blog.service';
 
     ])
   ],
-  providers: [BlogService],
+  providers: [BlogService,BlogHttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
